4451aa55a5dadfe036ddaac2229cebfe0820f2ef
Geena Fong <geena.fong@navexglobal.com>
2020-12-11 09:22:01 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "aws_amis" {
 }
 
 variable "key_name" {
-  description = "Name of the SSH keypair to use in AWS. 2"
+  description = "Name of the SSH keypair to use in AWS."
 }
 
 variable "aws_region" {
